4b6777fb842983470bee9f6b0307d998a81de6b7
dit02 <dit@imac-7.local>
2019-11-07 14:49:57 +0900

    Initial Commit

---
+++ CLLocation03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLLocation03
+//
+//  Created by dit02 on 2019. 11. 7..
+//  Copyright © 2019년 DIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
